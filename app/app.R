959ca24790efa9a7d200d5c4d46a9a88fd57e941
Anton Pohle <s0582388@htw-berlin.de>
2024-03-24 18:06:55 +0100

    add deployment

--- app/app.R
+++ app/app.R
@@ -11,29 +11,38 @@ ui <- fluidPage(
         tabPanel("Year 1", 
                  sliderInput("percentageOfUsersY1", "Percentage of Total Visitors Year 1:", min = 0, max = 5, value = 0.4, step = 0.1),
                  sliderInput("conversionRateY1", "Conversion Rate for Explorers Year 1 (%):", min = 0, max = 100, value = 2),
-                 sliderInput("bUsersY1", "Number of Businesses Year 1:", min = 0, max = 50, value = 15),
+                 sliderInput("bUsersY1", "Number of Businesses Year 1:", min = 0, max = 50, value = 1),
                  numericInput("bFeeY1", "Monthly Fee for Businesses Year 1 (€):", value = 50),
                  numericInput("addRevenueY1", "Monthly Advertisement Revenue Year 1 (€):", value = 2000),
                  numericInput("numCitiesY1", "Number of Cities Year 1:", value = 1),
-                 numericInput("totalCostY1", "Total Cost Year 1 (€):", value = 10000)
+                 numericInput("totalCostY1", "Total Cost Year 1 (€):", value = 650000)
         ),
         tabPanel("Year 2", 
                  sliderInput("percentageOfUsersY2", "Percentage of Total Visitors Year 2:", min = 0, max = 5, value = 0.4, step = 0.1),
                  sliderInput("conversionRateY2", "Conversion Rate for Explorers Year 2 (%):", min = 0, max = 100, value = 2),
-                 sliderInput("bUsersY2", "Number of Businesses Year 2:", min = 0, max = 50, value = 15),
+                 sliderInput("bUsersY2", "Number of Businesses Year 2:", min = 0, max = 50, value = 1),
                  numericInput("bFeeY2", "Monthly Fee for Businesses Year 2 (€):", value = 50),
                  numericInput("addRevenueY2", "Monthly Advertisement Revenue Year 2 (€):", value = 2000),
                  numericInput("numCitiesY2", "Number of Cities Year 2:", value = 0),
-                 numericInput("totalCostY2", "Total Cost Year 2 (€):", value = 10000)
+                 numericInput("totalCostY2", "Total Cost Year 2 (€):", value = 0)
         ),
         tabPanel("Year 3", 
                  sliderInput("percentageOfUsersY3", "Percentage of Total Visitors Year 3", min = 0, max = 5, value = 0.4, step = 0.1),
                  sliderInput("conversionRateY3", "Conversion Rate for Explorers Year 3 (%):", min = 0, max = 100, value = 2),
-                 sliderInput("bUsersY3", "Number of Businesses Year 3:", min = 0, max = 50, value = 15),
+                 sliderInput("bUsersY3", "Number of Businesses Year 3:", min = 0, max = 50, value = 1),
                  numericInput("bFeeY3", "Monthly Fee for Businesses Year 3 (€):", value = 50),
                  numericInput("addRevenueY3", "Monthly Advertisement Revenue Year 3 (€):", value = 2000),
                  numericInput("numCitiesY3", "Number of Cities Year 3:", value = 0),
-                 numericInput("totalCostY3", "Total Cost Year 3 (€):", value = 10000)
+                 numericInput("totalCostY3", "Total Cost Year 3 (€):", value = 0)
+        ),
+        tabPanel("Year 4", 
+                 sliderInput("percentageOfUsersY4", "Percentage of Total Visitors Year 4", min = 0, max = 5, value = 0.4, step = 0.1),
+                 sliderInput("conversionRateY4", "Conversion Rate for Explorers Year 4 (%):", min = 0, max = 100, value = 2),
+                 sliderInput("bUsersY4", "Number of Businesses Year 4:", min = 0, max = 50, value = 1),
+                 numericInput("bFeeY4", "Monthly Fee for Businesses Year 4 (€):", value = 50),
+                 numericInput("addRevenueY4", "Monthly Advertisement Revenue Year 4 (€):", value = 2000),
+                 numericInput("numCitiesY4", "Number of Cities Year 4:", value = 0),
+                 numericInput("totalCostY4", "Total Cost Year 4 (€):", value = 0)
         )
      
       )
@@ -43,8 +52,10 @@ ui <- fluidPage(
         id = "yearOutputTabs",
         tabPanel("Year 1 Revenue", plotOutput("revenuePlotY1")),
         tabPanel("Year 2 Revenue", plotOutput("revenuePlotY2")),
-        tabPanel("Year 3 Revenue", plotOutput("revenuePlotY3"))
-      )
+        tabPanel("Year 3 Revenue", plotOutput("revenuePlotY3")),
+        tabPanel("Year 4 Revenue", plotOutput("revenuePlotY4"))
+      ),
+      plotOutput("profitPlot")
     )
   )
 )
@@ -60,7 +71,7 @@ server <- function(input, output) {
       addRevenue <- input[[paste0("addRevenueY", year)]]
       numCities <- input[[paste0("numCitiesY", year)]]
       totalCost <- input[[paste0("totalCostY", year)]]
-      
+      #note: i am using 2.7 mllion to roughly include the local population
       aUsers <- 2700000 * (percentageOfUsers / 100) * (conversionRate / 100)
       aRevenue <- round(aUsers * 3.99 * 12) * numCities
       bRevenue <- round(bUsers * bFee * 12) * numCities
@@ -87,6 +98,55 @@ server <- function(input, output) {
   output$revenuePlotY1 <- renderRevenuePlotForYear(1)
   output$revenuePlotY2 <- renderRevenuePlotForYear(2)
   output$revenuePlotY3 <- renderRevenuePlotForYear(3)
+  output$revenuePlotY4 <- renderRevenuePlotForYear(4)
+  
+  output$profitPlot <- renderPlot({
+
+    totalRevenueAllYears <- 0
+    totalCostAllYears <- 0
+    
+
+    for (year in 1:4) {  
+      percentageOfUsers <- input[[paste0("percentageOfUsersY", year)]]
+      conversionRate <- input[[paste0("conversionRateY", year)]]
+      bUsers <- input[[paste0("bUsersY", year)]]
+      bFee <- input[[paste0("bFeeY", year)]]
+      addRevenue <- input[[paste0("addRevenueY", year)]]
+      numCities <- input[[paste0("numCitiesY", year)]]
+      totalCost <- input[[paste0("totalCostY", year)]]
+      
+      aUsers <- 2700000 * (percentageOfUsers / 100) * (conversionRate / 100)
+      aRevenue <- round(aUsers * 3.99 * 12) * numCities
+      bRevenue <- round(bUsers * bFee * 12) * numCities
+      adRevenue <- round(addRevenue * 12) * numCities
+      yearlyTotalRevenue <- aRevenue + bRevenue + adRevenue
+      
+
+      totalRevenueAllYears <- totalRevenueAllYears + yearlyTotalRevenue
+      totalCostAllYears <- totalCostAllYears + totalCost
+    }
+    
+
+    netProfit <- totalRevenueAllYears - totalCostAllYears
+    netProfitColor <- ifelse(netProfit >= 0, "black", "red")
+    
+ 
+    financialData <- data.frame(
+      Category = factor(c("Total Cost", "Total Revenue", "Net Profit"),
+                        levels = c("Total Cost", "Total Revenue", "Net Profit")),
+      Amount = c(-totalCostAllYears, totalRevenueAllYears, netProfit),  # Negative cost for visual distinction
+      Color = c("orange2", "deepskyblue", netProfitColor)  # Color for each category
+    )
+    
+    ggplot(financialData, aes(x = Category, y = Amount, fill = Color)) +
+      geom_bar(stat = "identity", position = position_dodge(), show.legend = FALSE) +
+      theme_minimal() +
+      scale_fill_identity() +
+      labs(title = "Financial Overview Across All Years", y = "Amount (€)", x = "") +
+      geom_text(aes(label = format(round(Amount), big.mark = ",")),
+                position = position_dodge(width = 0.9), vjust = -0.25)
+  })
+  
 }
 
 # Run the application
